fc6144adb7d017a0b523acb0960f645a45906cda
Davi Mello <dsmello.9@gmail.com>
2017-11-07 16:12:34 +0000

    Odometry script, v0.1 (Just the basic)

---
+++ Scripts/Real/odometry_rosaria.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import rospy
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Point, Pose, Quaternion, Twist, Vector3
+
+import rosaria
+import robot_pose_ekf
+
+
+# def callbackrosaria():
+#     print rosaria
+
+
+def callback(msg):
+    x = msg.pose.pose.position.x
+    y = msg.pose.pose.position.y
+    rospy.loginfo("x: {}, y: {}".format(x, y))
+
+def main():
+    rospy.init_node('location_monitor')
+    rospy.Subscriber("/odom", Odometry, callback)
+    rospy.spin()
+
+if __name__ == '__main__':
+    main()
+
+
+

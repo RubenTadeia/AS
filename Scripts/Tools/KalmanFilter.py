030070d4b45e337ce87a9567c60bb83d68730853
Davi Mello <dsmello.9@gmail.com>
2017-11-28 15:57:21 +0000

    # This code not is my i find on some lost forum, but works fine, i can made any adjustment if someone needs.
    pls , just tell what variables you need to read, and what format (list, array, str ...)
    
    possibly the most simplest method is join all code inside of one script or the best method is to work with streaming. For different threads do onw jobs.

---
+++ Scripts/Tools/KalmanFilter.py
@@ -0,0 +1,51 @@
+# coding=utf-8
+import numpy as np
+
+
+# The Error from Laser is 30 mm
+
+class KF(object):
+
+    KalmanGain = 0
+    Estimativa = 0
+    ErroDaEstimativa = 0
+
+    def __init__ (self, ValorEstimado, ErroDoValorEstimado, ValorMedido, ErroDoValorMedido, ValorEstimadoAnterior, ErroDoValorEstimadoAnterior):
+
+        self.ValorEstimado = ValorEstimado
+        self.ErroDoValorEstimado = ErroDoValorEstimado
+
+        self.ValorMedido = ValorMedido
+        self.ErroDoValorMedido = ErroDoValorMedido
+
+        self.ValorEstimadoAnterior = ValorEstimadoAnterior
+        self.ErroDoValorEstimadoAnterior = ErroDoValorEstimadoAnterior
+
+    # def __kalmangain__ (self):
+
+        self.KalmanGain = self.ErroDoValorEstimado / (self.ErroDoValorEstimado + self.ErroDoValorMedido)
+
+    # def __estimativa__(self):
+
+        self.Estimativa = self.ValorEstimado + self.KalmanGain * (self.ValorMedido - self.ValorEstimado)
+
+    # def __errodaestimativa__(self):
+
+        self.ErroDaEstimativa = (1 - self.KalmanGain) * self.ErroDoValorEstimadoAnterior
+
+        """
+        Atualiza a função com base nos valores já criados.
+        """
+
+    def update(self, ValorEstimado, ErroDoValorEstimado, ValorMedido, ErroDoValorMedido):
+
+        self.__init__(ValorEstimado, ErroDoValorEstimado, ValorMedido, ErroDoValorMedido, self.Estimativa, self.ErroDaEstimativa)
+
+    def getEstimativa(self):
+
+        return self.Estimativa
+
+    def getErroDaEstimativa(self):
+
+        return self.ErroDaEstimativa
+

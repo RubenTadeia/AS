cd42fcad2ac2f689c63b1ef533a2c0ff7897898a
Davi Mello <dsmello.9@gmail.com>
2017-11-30 01:57:23 +0000

    Kalman Filter

--- Scripts/Tools/KalmanFilter.py
+++ Scripts/Tools/KalmanFilter.py
@@ -5,13 +5,13 @@ import numpy as np
 # The Error from Laser is 30 mm
 
 
-class KF(object):
+class KalmanFilter(object):
 
     KalmanGain = 0
     Estimativa = 0
     ErroDaEstimativa = 0
 
-    def __init__ (self, ValorEstimado, ErroDoValorEstimado, ValorMedido, ErroDoValorMedido, ValorEstimadoAnterior, ErroDoValorEstimadoAnterior):
+    def __init__(self, ValorEstimado, ErroDoValorEstimado, ValorMedido, ErroDoValorMedido, ValorEstimadoAnterior, ErroDoValorEstimadoAnterior):
 
         self.ValorEstimado = ValorEstimado
         self.ErroDoValorEstimado = ErroDoValorEstimado
@@ -50,3 +50,30 @@ class KF(object):
 
         return self.ErroDaEstimativa
 
+
+Valor1 = np.array([16.5, 10])
+Valor1Error = np.array([2, 1])
+Valor2 = np.array([15, 11])
+Valor2Error = np.array([0.5, 0.1])
+Valor3 = np.array([10, 6])
+Valor3Error = np.array([1, 0.5])
+
+kf = KalmanFilter(Valor1, Valor1Error, Valor2, Valor2Error, Valor3, Valor3Error)
+
+print(kf.getEstimativa())
+
+Valor1 = np.array([33, 10])
+Valor1Error = np.array([2, 1])
+Valor2 = np.array([30, 11])
+Valor2Error = np.array([0.5, 0.1])
+
+kf.update(Valor1, Valor1Error, Valor2, Valor2Error)
+print(kf.getEstimativa())
+
+Valor1 = np.array([85, 10])
+Valor1Error = np.array([2, 1])
+Valor2 = np.array([91, 11])
+Valor2Error = np.array([0.5, 0.1])
+
+kf.update(Valor1, Valor1Error, Valor2, Valor2Error)
+print(kf.getEstimativa())
